c536e8bde37d7ceebf08ea949120ca97359f6210
GN0MI0 <103611243+SauloJose@users.noreply.github.com>
2022-06-30 21:42:46 -0300

    Update sumofiles.ino

--- sumofiles.ino
+++ sumofiles.ino
@@ -1,21 +1,78 @@
-#include "sumoInfo.h"
+#include "sumoinfo.h"
 //Implementação das funções
-
-/*================|| Funçõoes ||====================*/
-
+/*==================================|| DEFINIÇÃO DAS FUNÇÕES ||==============================*/
 //Ativar pulso do sensor ultrasônico
-float Ultrassonic(){
-  
+float Ultrassonic() {
+  digitalWrite(TRIG,LOW);
+  vTaskDelay(3/15);
+  digitalWrite(TRIG,HIGH);
+  vTaskDelay(10/15);
+  digitalWrite(TRIG,LOW)
+
+  return pulseIn(ECHO,HIGH);
 }
 
 //Converter leitura para distância
-float convertCm(){
-  
+float convertCm() {
+  long tempo = Ultrassonic();
+  float dist;
+  tempo=tempo/2;
+  dist=tempo*0.034; //Convertendo em cm
+
+  return dist;
 }
 
 //Rotina do sensor.
-void routineMotor(uint16_t t){
-  
+void routineMotor(uint16_t t) {
+
 }
 
-/*================|| MÉTODOS ||===================*/===*/
+/*====================================|| DEFINIÇÃO DOS TAREFAS ||==============================*/
+//Tarefa para tomar decisões
+void vTaskBrain(void* pvParamaters) {
+  (void*) pvParamaters;
+  while (1) {
+
+  }
+}
+
+//Tarefa para coletar dados dos sensores
+void vTaskSensor(void* pvParamaters) {
+  (void*) pvParamaters;
+
+  //Variáveis para enviar ao cérebro.
+  float distance;
+  bool sensors[4];
+  
+  while (1) {
+    //Analisando dados do sensor ultrassÔnico
+  }
+}
+
+
+//Tarefa de ligar e desligar o robô
+void vTaskOnOFF(void* pvParamaters) {
+  (void*) pvParamaters;
+  while (1) {
+    //Só faz algo quando um semáforo for liberado por uma interrupção.
+  }
+}
+
+
+//Tarefa que irá monitorar o comportamento desejado.
+void vTaskMotor(void* pvParamaters) {
+  (void*) pvParamaters;
+  while (1) {
+
+  }
+}
+
+
+/*=====================================|| DEFINIÇÃO DOS MÉTODOS ||==============================*/
+
+//Métodos da classe motor
+
+
+
+
+//Método da classe robÔ
